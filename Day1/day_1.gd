adf166509992315b086e90aa585de863f733adb5
robro <433665+robro@users.noreply.github.com>
2024-12-01 10:36:02 -0800

    Add Day 1 part two and refactor

--- Day1/day_1.gd
+++ Day1/day_1.gd
@@ -1,30 +1,50 @@
 extends Node2D
 
 @onready var input_path : String = "res://Day1/input.txt"
+var left_list : Array[int]
+var right_list : Array[int]
 
 func _ready() -> void:
-	var input_file := FileAccess.open(input_path, FileAccess.READ)
-	part_one(input_file)
+	print("Day 1")
+	part_one()
+	part_two()
 
 
-func part_one(input_file: FileAccess) -> void:
-	var list_left : Array[int]
-	var list_right : Array[int]
+func part_one() -> void:
+	var total_distance := 0
+
+	set_lists()
+	left_list.sort()
+	right_list.sort()
+
+	for i in left_list.size():
+		total_distance += abs(left_list[i] - right_list[i])
+
+	print("Total distance: ", total_distance)
+
+
+func part_two() -> void:
+	var similarity_score := 0
+
+	set_lists()
+
+	for num in left_list:
+		similarity_score += num * right_list.count(num)
+
+	print("Similarity Score: ", similarity_score)
+
+
+func set_lists() -> void:
 	var line_string : String
 	var nums : PackedStringArray
-	var total_distance : int = 0
+	var input_file := FileAccess.open(input_path, FileAccess.READ)
+
+	left_list.clear()
+	right_list.clear()
 
 	while !input_file.eof_reached():
 		line_string = input_file.get_line()
 		if !line_string.is_empty():
 			nums = line_string.split(" ", false)
-			list_left.append(int(nums[0]))
-			list_right.append(int(nums[1]))
-
-	list_left.sort()
-	list_right.sort()
-
-	for i in list_left.size():
-		total_distance += abs(list_left[i] - list_right[i])
-
-	print("Total distance: ", total_distance)
+			left_list.append(int(nums[0]))
+			right_list.append(int(nums[1]))
